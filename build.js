b2cd47783072de0afafc64692115d0a868805aed
pp253 <pp.pp253@gmail.com>
2017-12-31 00:05:37 +0800

    update

--- build.js
+++ build.js
@@ -1,22 +1,29 @@
 
 var Metalsmith = require('metalsmith')
 var markdown = require('metalsmith-markdown')
+var jade = require('metalsmith-jade')
 var layouts = require('metalsmith-layouts')
 var permalinks = require('metalsmith-permalinks')
 
+const metadata = {
+  title: '2018清大工程領袖營',
+  description: "It's about saying »Hello« to the World.",
+  url: "https://2018ieemcamp.me/"
+}
+
 Metalsmith(__dirname)
-  .metadata({
-    title: '2018清大工程領袖營',
-    description: "It's about saying »Hello« to the World.",
-    url: "https://2018ieemcamp.me/"
-  })
-  .source('./src')
+  .metadata(metadata)
+  .source('./src/view')
   .destination('./')
   .clean(false)
+  .use(jade({
+    locals: metadata
+  }))
   .use(markdown())
   .use(permalinks())
   .use(layouts({
-    engine: 'handlebars'
+    engine: 'jade',
+    directory: './src/templates'
   }))
   .build(function (err, files) {
     if (err) { throw err; }
