79870c8a3ffdd44af671bb0376ede6164c14ddaf
pp253 <pp.pp253@gmail.com>
2017-12-30 21:51:55 +0800

    first step

---
+++ build.js
@@ -0,0 +1,23 @@
+
+var Metalsmith = require('metalsmith')
+var markdown = require('metalsmith-markdown')
+var layouts = require('metalsmith-layouts')
+var permalinks = require('metalsmith-permalinks')
+
+Metalsmith(__dirname)
+  .metadata({
+    title: '2018清大工程領袖營',
+    description: "It's about saying »Hello« to the World.",
+    url: "https://2018ieemcamp.me/"
+  })
+  .source('./src')
+  .destination('./build')
+  .clean(false)
+  .use(markdown())
+  .use(permalinks())
+  .use(layouts({
+    engine: 'handlebars'
+  }))
+  .build(function (err, files) {
+    if (err) { throw err; }
+  });
